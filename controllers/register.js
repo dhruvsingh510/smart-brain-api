71de2b9a262e87f093a4f0fb22ff0ed5481b5ef0
Dhruv Singh <dhruvsingh510@gmail.com>
2020-11-07 18:19:21 +0530

    changing error message

--- controllers/register.js
+++ controllers/register.js
@@ -26,7 +26,7 @@ const handleRegister = (req, res, db, bcrypt) => {
       .then(trx.commit)
       .catch(trx.rollback)
     })
-    .catch(err => res.status(400).json('unable to register 11111'))
+    .catch(err => res.status(400).json('unable to register'))
 }
 
 module.exports = {
