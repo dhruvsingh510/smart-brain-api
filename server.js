51f14150b04787b9444725553d4c84c44b76ce69
dhruvsingh510 <dhruvsingh510@gmail.com>
2023-09-18 20:51:54 +0300

    adding Docker and Redis

--- server.js
+++ server.js
@@ -1,39 +1,48 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser'); // latest version of exressJS now comes with Body-Parser!
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
+const morgan = require('morgan');
 
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
+const auth = require('./controllers/authorization');
 
+//Database Setup
 const db = knex({
-  // connect to your own database here
   client: 'pg',
-  connection: {
-    host : 'smart-brain-database.czs7rg8gbm80.ap-south-1.rds.amazonaws.com',
-    port : 5432,
-    user : 'root',
-    password : 'TODaKS2W5XymEO6ICGIc',
-    database : 'postgres'
-  }
+  connection: process.env.POSTGRES_URI 
 });
 
 const app = express();
 
-app.use(cors())
-app.use(bodyParser.json());
+const whitelist = ['http://localhost:3001']
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
 
-app.get('/', (req, res)=> { res.send(db.users) })
-app.post('/signin', signin.handleSignin(db, bcrypt))
+
+app.use(morgan('combined'));
+app.use(cors(corsOptions))
+app.use(express.json()); // latest version of exressJS now comes with Body-Parser!
+
+app.post('/signin', signin.signinAuthentication(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
-app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
-app.put('/image', (req, res) => { image.handleImage(req, res, db)})
-app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
+app.get('/profile/:id', auth.requireAuth, (req, res) => { profile.handleProfileGet(req, res, db)})
+app.post('/profile/:id', auth.requireAuth, (req, res) => { profile.handleProfileUpdate(req, res, db)})
+app.put('/image', auth.requireAuth, (req, res) => { image.handleImage(req, res, db)})
+app.post('/imageurl', auth.requireAuth, (req, res) => { image.handleApiCall(req, res)})
 
-app.listen(3000, function(){
-  console.log('Express listening on port', this.address().port);
-});
+app.listen(3000, ()=> {
+  console.log('app is running on port 3000');
+})
 
